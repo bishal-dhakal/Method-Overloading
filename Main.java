0bf138957994e26eb9dbdf36012c40ca6a753de9
bishal <46195146+bishal-dhakal@users.noreply.github.com>
2023-02-25 06:16:17 +0545

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Add("hello","World"));
+    }
+
+    public static int Add(int a, int b){
+        return (a+b) ;
+    }
+
+    public static double Add(double a, double b){
+        return (a+b) ;
+    }
+
+    public static String Add(String a, String b){
+        return (a+b);
+    }
+
+}
